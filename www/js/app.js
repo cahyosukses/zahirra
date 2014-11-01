e2e1738b9351a344278504788c455a727b0989f6
Dikdik Tasdik Laksana <dikdik.zahirra@gmail.com>
2014-11-01 12:59:14 +0700

    update search data

--- www/js/app.js
+++ www/js/app.js
@@ -1,68 +1,57 @@
-angular.module('zahirra', ['ionic', 'starter.controllers'])
+var app = angular.module('zahirra', ['ionic', 'starter.controllers'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-
-    .state('app', {
-      url: "/app",
-      abstract: true,
-      templateUrl: "templates/menu.html",
-      controller: 'AppCtrl'
-    })
-
-    .state('app.search', {
-      url: "/search",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/search.html"
+app.run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
         }
-      }
-    })
-  
-    .state('app.cart', {
-      url: "/cart",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/cart.html"
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
         }
-      }
-    })  
-
-    .state('app.products', {
-      url: "/products",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/products.html",
-          controller: 'ProductsCtrl'
-        }
-      }
-    })
-
-    .state('app.single', {
-      url: "/products/:productId",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/product.html",
-          controller: 'ProductCtrl'
-        }
-      }
     });
-    
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/products');
 });
 
+app.config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider.state('app', {
+        url: "/app",
+        abstract: true,
+        templateUrl: "templates/menu.html",
+        controller: 'AppCtrl'
+    }).state('app.search', {
+        url: "/search",
+        views: {
+            'menuContent': {
+                templateUrl: "templates/search.html"
+            }
+        }
+    }).state('app.cart', {
+        url: "/cart",
+        views: {
+            'menuContent': {
+                templateUrl: "templates/cart.html"
+            }
+        }
+    }).state('app.products', {
+        url: "/products",
+        views: {
+            'menuContent': {
+                templateUrl: "templates/products.html",
+                controller: 'ProductsCtrl'
+            }
+        }
+    }).state('app.single', {
+        url: "/products/:productId",
+        views: {
+            'menuContent': {
+                templateUrl: "templates/product.html",
+                controller: 'ProductCtrl'
+            }
+        }
+    });
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/products');
+});
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,46 +1,69 @@
 angular.module('starter.controllers', ['ionic'])
-
-.controller('AppCtrl', function($scope) {
-    $scope.search;    
+        .controller('AppCtrl', function($scope) {
 })
-
-.controller('ProductsCtrl', function($scope, $http, $ionicLoading) {        
+        .controller('ProductsCtrl', function($scope, $http, $ionicLoading) {
 //    this.getProducts = function($scope){
     $ionicLoading.show({
         template: 'Loading...'
     });
-    
-      $http.get("http://zahirra.com/android/products/load_lists")
-      .success(function(result){
-          $scope.products = result;
-          $scope.$broadcast("scroll.refreshComplete");
-          $ionicLoading.hide();
-      });
+
+    $http.get("http://zahirra.com/android/products/load_lists")
+            .success(function(result) {
+        $scope.products = result;
+        $scope.$broadcast("scroll.refreshComplete");
+        $ionicLoading.hide();
+    });
 //    }
 })
 
-.controller('ProductCtrl', function($http, $scope, $stateParams, $ionicLoading) {
-    var id = $stateParams.productId;    
+        .controller('ProductCtrl', function($http, $scope, $stateParams, $ionicLoading) {
+    var id = $stateParams.productId;
     $ionicLoading.show({
         template: 'Loading...'
     });
-    
+
     $http.get("http://zahirra.com/android/products/getDetailProduct/" + id)
-        .success(function(result){
-            $scope.product = result;
-            $ionicLoading.hide();
+            .success(function(result) {
+        $scope.product = result;
+        $ionicLoading.hide();
     });
 })
 
-.controller('SearchProductCtrl', function($http, $scope, $stateParams, $ionicLoading) {
-    var id = $stateParams.productId;    
+        .controller('SearchProductCtrl', function($http, $scope, $stateParams, $ionicLoading) {
+    var id = $stateParams.productId;
     $ionicLoading.show({
         template: 'Loading...'
     });
-    
+
     $http.get("http://zahirra.com/android/products/getDetailProduct/" + id)
-        .success(function(result){
-            $scope.product = result;
-            $ionicLoading.hide();
+            .success(function(result) {
+        $scope.product = result;
+        $ionicLoading.hide();
     });
 });
+
+function SearchCtrl($scope, $http, $ionicLoading) {
+    $scope.url = 'http://zahirra.com/android/products/SearchProducts'; // The url of our search
+
+    // The function that will be executed on button click (ng-click="search()")
+    $scope.search = function() {
+        $ionicLoading.show({
+            template: 'Loading...'
+        });
+        // Create the http post request
+        // the data holds the keywords
+        // The request is a JSON request.
+        $http.get($scope.url, {"data": $scope.keywords}).success(function(result) {
+            $scope.products = result;
+//            $scope.status = status;
+//            $scope.data = data;
+//            $scope.result = data; // Show result from server in our <pre></pre> element
+            $ionicLoading.hide();
+        }).error(function(data, status) {
+            $scope.data = data || "Request failed";
+            $scope.status = status;
+        });
+    };
+}
+
+
