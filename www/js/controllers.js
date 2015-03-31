55bbb345de65dde9db62c7365037f27da885ae3d
Dikdik Tasdik Laksana <dikdik.zahirra@gmail.com>
2015-03-31 11:52:35 +0700

    update share button

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -215,7 +215,7 @@ angular.module('starter.controllers', ['ionic']).controller('AppCtrl', function(
     $ionicLoading.show({
         template: 'Loading...'
     });
-    $http.get("http://zahirra.com/android/products/add_to_cart/" + id).success(function(result) {
+    $http.get("http://zahirra.com/android/products/load_cart/" + id).success(function(result) {
         $scope.carts = result;
         $ionicLoading.hide();
         console.log('Add to Cart By ID ' + id);
